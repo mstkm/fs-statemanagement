2ecefa12646b6b53b3991fccfd02ddc41e6aadad
mstkm <mustakimmugi@gmail.com>
2026-09-24 10:23:26 +0700

    exercise 6.19 - 6.23

---
+++ query-anecdotes/src/hooks/useAnecdotes.js
@@ -0,0 +1,40 @@
+import { useQueryClient, useQuery, useMutation } from "@tanstack/react-query"
+import { createAnecdote, getAnecdotes, updateAnecdote } from "../requests"
+
+export const useAnecdotes = () => {
+  const queryClient = useQueryClient()
+
+  const result = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: getAnecdotes,
+    retry: 1,
+    refetchOnWindowFocus: false
+  })
+
+  const newAnecdoteMutation = useMutation({
+    mutationFn: createAnecdote,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['anecdotes'] })
+    }
+  })
+
+  const updateAnecdoteMutation = useMutation({
+    mutationFn: updateAnecdote,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['anecdotes'] })
+    },
+  })
+
+  return {
+    anecdotes: result.data,
+    isPending: result.isPending,
+    isError: result.isError,
+    addAnecdote: (content) => newAnecdoteMutation.mutate({ content, votes: 0 }),
+    voteAnecdote: (anecdote) => {
+      updateAnecdoteMutation.mutate({
+        ...anecdote,
+        votes: anecdote.votes + 1
+      })
+    }
+  }
+}
